5da50a03116cf44e202f4ee4f04077e7f897d35f
Hugo Lefèvre <93387947+hlefevregit@users.noreply.github.com>
2025-01-15 14:50:43 +0100

    cd works

--- srcs/buildins/cd.c
+++ srcs/buildins/cd.c
@@ -6,7 +6,7 @@
 /*   By: hulefevr <hulefevr@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 15:20:55 by hulefevr          #+#    #+#             */
-/*   Updated: 2025/01/14 20:09:36 by hulefevr         ###   ########.fr       */
+/*   Updated: 2025/01/15 14:50:09 by hulefevr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,10 @@ static void	move_to_dir(char **env, char *path)
 		return ;
 	}
 	if (getcwd(buffer, BUFSIZ) == 0)
-	{
-		printf("something went wrong\n");
-		move_to_dir(env, get_var_from_env(env, "OLDPWD") + 7);
-	}
+		move_to_dir(env, "..");
 	set_env_var(env, "OLDPWD", oldpwd);
 	newpwd = getcwd(NULL, 0);
 	set_env_var(env, "PWD", newpwd);
-	
 	free(newpwd);
 }
 
@@ -68,36 +64,3 @@ void	ft_cd(t_cmd *cmd)
 		path = cmd->args[1];
 	move_to_dir(cmd->data->env, path);
 }
-
-// void ft_cd(t_cmd *cmd)
-// {
-// 	char	buffer[BUFSIZ];
-// 	char	**av;
-
-// 	av = cmd->args;
-// 	if (av[1] == NULL)
-// 	{
-// 		if (chdir(find_in_env("HOME", envp)) != 0)
-// 		{
-// 			perror("cd");
-// 		}
-// 	}
-// 	else if (ft_strcmp(av[1], "-") == 0)
-// 	{
-// 		if (chdir(find_in_env("OLDPWD", mini.envp)) != 0)
-// 		{
-// 			perror("cd");
-// 		}
-// 	}
-// 	else if (chdir(av[1]) != 0)
-// 	{
-// 		perror("cd");
-// 	}
-// 	else if (getcwd(buffer, BUFSIZ) == 0)
-// 	{
-// 		printf("NIQUE TA MERE TOI\n");
-// 		ft_cd(ft_split("cd ..", 32), mini);
-// 	}
-// 	else
-// 		g_global.exit_status = EXIT_SUCCESS;
-// }
